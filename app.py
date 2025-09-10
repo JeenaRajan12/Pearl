69f6f0e28d667abedd4ef130d1817f11c3278796
JeenaRajan12 <jeenarajan9746@gmail.com>
2025-09-10 21:38:01 +0530

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("Welcome to concole")
+# Two numbers adding program
+# Get input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Add the numbers
+sum_result = num1 + num2
+# Display the result
+print("The sum is:", sum_result)
